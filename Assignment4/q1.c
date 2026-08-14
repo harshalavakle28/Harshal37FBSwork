1e7f127783e6e3b7efe857c7fdb0dfc69f109e00
harshalavakle28 <avakaleharshal@gmail.com>
2026-08-14 22:46:43 +0530

    Add files via upload

---
+++ Assignment4/q1.c
@@ -0,0 +1,19 @@
+void evenodd();
+void main() {
+	evenodd();
+	
+}
+void evenodd(){
+	int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+	if(num%2==0)
+	{
+		printf("The number is even.");
+	}
+	else
+      	printf("The number is odd.");
+	
+	
+}  
